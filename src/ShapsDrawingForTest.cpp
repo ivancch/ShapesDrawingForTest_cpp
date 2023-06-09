932d058abb36a52f23f50cab07964a0347705a1c
Ivan- <76176527+ivancch@users.noreply.github.com>
2023-06-10 01:36:49 +0300

    first commit

---
+++ src/ShapsDrawingForTest.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : ShapsDrawingForTest.cpp
+// Author      : ivan-ch
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <conio.h>
+
+int main() {
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
+	_getch();
+	return 0;
+}
